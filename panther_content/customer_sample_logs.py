01b3ff0f3cdbfa19baf09384e3e1a4075b81f382
Carrie Pascale <carrie.pascale@panther.io>
2023-01-04 17:51:57 -0500

    initial commit

--- panther_content/customer_sample_logs.py
+++ panther_content/customer_sample_logs.py
@@ -1,27 +1,27 @@
 import json
 
 my_custom_sample = json.dumps(
-      {
+    {
         "id": {
-          "applicationName": "login",
+            "applicationName": "login",
         },
         "actor": {
-          "email": "some.user@somedomain.com",
+            "email": "some.user@somedomain.com",
         },
         "type": "login",
         "name": "login_failure",
-      }
+    }
 )
 
 acme_sample = json.dumps(
-      {
+    {
         "id": {
-          "applicationName": "login",
+            "applicationName": "login",
         },
         "actor": {
-          "email": "some.user@acme.com",
+            "email": "some.user@acme.com",
         },
         "type": "login",
         "name": "login_failure",
-      }
+    }
 )
